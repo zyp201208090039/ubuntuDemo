9316b7aaa480752af32f3fa69d8119b459e93637
yapeng <2672546346@qq.com>
2021-02-25 06:22:33 -0800

    initial ubuntu

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "myfile.h"
+int main()
+{
+  say();
+  printf("this is main.c function");
+}
---
+++ myfile.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "myfile.h"
+void say()
+{
+ printf("this is saying");
+}
